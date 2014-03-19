57a55ba81e45029f601c8b58a991bdcc8bdaa4b1
Edward Thomason <edward@thomason.me.uk>
2014-03-19 17:38:09 +0000

    removed test for method that no longer exists

--- unittests/nav_bar_sanity.py
+++ unittests/nav_bar_sanity.py
@@ -13,19 +13,6 @@ class NavBarSanity(unittest.TestCase):
         self.nav_bar.accessor = self.test_accessor
         self.test_accessor.reset_all()
 
-    def test_get_dash_names(self):
-        """
-        tests that the object inheretance works as expected
-        """
-        dashboards = self.nav_bar.get_dashboards()
-        self.assertEquals(len(dashboards), 0)
-        test_dash_name = "test-dash-name"
-        self.test_accessor.add_document_to_collection("dashboard", {"name": test_dash_name})
-
-        dashboards = self.nav_bar.get_dashboards()
-        self.assertEquals(len(dashboards), 1)
-        self.assertIn(test_dash_name, dashboards)
-
 
 if __name__ == '__main__':
     unittest.main()
